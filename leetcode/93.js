810ee4cd7b545cdc3a888744e2d7c8e5bc5ea1f2
divasatanica <coma.desperao@gmail.com>
2021-12-28 17:36:27 +0800

    doc: 补充 LC 93 的代码注释

--- leetcode/93.js
+++ leetcode/93.js
@@ -12,8 +12,9 @@ var restoreIpAddresses = function(s) {
     }
     let path = [];
     let split = 0;
+    console.time(`Restore ${s}`)
     dfs(s, s.length, split, 0, path, res);
-
+    console.timeEnd(`Restore ${s}`)
     return res;
 };
 
@@ -37,6 +38,9 @@ function isValidIPSegment(s, begin, end) {
 }
 
 function dfs(s, len, split, begin, path, res) {
+    // 如果开始位置超出有效范围
+    // 判断 split 是否为 4 (IP 段最多为 4 段)
+    // 拼接出一条有效结果推入 res 数组
     if (begin === len) {
         if (split === 4) {
             res.push(path.join('.'));
@@ -44,21 +48,34 @@ function dfs(s, len, split, begin, path, res) {
         return;
     }
 
-    if (len - begin < (4 - split) || len - begin > 3 * (4 - split)) {
+    // 剩下的可以分割的数字,分别讨论最小和最大两个边界
+    // 最小即为 (4 - 当前已分割段数) * 1,即后面待分割的段全都是 1 位数的
+    // 最大即为 (4 - 当前已分割段数) * 3,即后面待分割的段全都是 3 位数的
+    // 剩下的可以分割的数字位数,必须在最小和最大的边界形成的区间以内(闭区间)
+    // 否则认为无法完成分割,直接 return
+    if (len - begin < 1 * (4 - split) || len - begin > 3 * (4 - split)) {
         return;
     }
 
-    for (let i = 0; i <= 3; i ++) {
+    // 尝试分割到 3 位数的情况 (因为 IP 段最长为 3 位数)
+    for (let i = 0; i < 3; i ++) {
         if (begin + i >= len) {
             break;
         }
         let ipSegment = isValidIPSegment(s, begin, begin + i);
         if (ipSegment >= 0) {
+            // 若为有效的 IP 段,使用 path 暂存
+            // 递归调用 dfs 函数继续往下分割
             path.push(ipSegment);
+            // split 已分割段数 + 1
+            // begin 位置需要加上当前循环内导致的偏移量
             dfs(s, len, split + 1, begin + i + 1, path, res);
+            // 递归过程返回后,表示后续可能的分割已经完成
+            // 将上面暂存的段丢弃,将当前段多分割一位数进行一轮新的尝试
             path.pop();
         }
     }
 }
 
-console.log(restoreIpAddresses('25525511135'))
+console.log(restoreIpAddresses('25525111235'))
+console.log(restoreIpAddresses('11112211'))
